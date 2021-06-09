5ba2d2c6ea4b596be11a5ab9164dad4ffd76cc6c
Kenneth <kmadjei25@gmail.com>
2021-06-09 04:47:35 +0000

    Initial commit of project files

---
+++ app.py
@@ -0,0 +1,35 @@
+# accessing modules required to run the app
+import os
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
+if os.path.exists("env.py"):
+    import env
+
+# Initialize instance of the Flask App 
+app = Flask(__name__)
+
+# set the config Key & values of the flask app
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+# gets secret key for Flash message object
+app.secret_key = os.environ.get("SECRET_KEY")
+
+# connects the flask app to the MongoDB server
+mongo = PyMongo(app)
+
+
+# binds the get_tasks function to the default routing / url view of the app
+@app.route("/")
+def home():
+    pass
+
+
+# runs the flask application as the main module
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
