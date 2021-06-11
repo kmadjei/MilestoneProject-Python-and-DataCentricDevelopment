258c36d44f43c6f9dbd7797cc54b81e9a73e3601
Kenneth <kmadjei25@gmail.com>
2021-06-11 00:04:46 +0000

    added CRUD functionality templates , completed frontend design for recipes.html, and updated style.css

--- app.py
+++ app.py
@@ -12,13 +12,14 @@ if os.path.exists("env.py"):
 app = Flask(__name__)
 
 # set the config Key & values of the flask app
-# app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
-# app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+
 # gets secret key for Flash message object
 app.secret_key = os.environ.get("SECRET_KEY")
 
 # connects the flask app to the MongoDB server
-# mongo = PyMongo(app)
+mongo = PyMongo(app)
 
 
 # binds the home function to the default url route / view page
@@ -29,7 +30,37 @@ def home():
 
 @app.route('/recipes')
 def get_recipes():
-    return render_template('recipes.html')
+    categories = list(mongo.db.food_categories.find())
+
+    
+    #loop to find list of matching categories and display none for 0 list
+
+    return render_template('recipes.html', categories = categories)
+
+@app.route('/recipes/<category>')
+def get_food_category(category):
+    pass
+
+
+@app.route('/recipes/list')
+def show_all_recipe():
+    pass
+
+@app.route('/delete_category')
+def delete_category():
+    pass
+
+@app.route('/edit_category')
+def delete_category():
+    pass
+
+@app.route('/delete_recipe')
+def edit_recipe():
+    pass
+
+@app.route('/edit_recipe')
+def edit_recipe():
+    pass
 
 
 # runs the flask application as the main module
