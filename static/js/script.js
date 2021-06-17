8fe4dd462b1e43a07ce21b744d35888190263f2d
Kenneth <kmadjei25@gmail.com>
2021-06-17 23:00:06 +0000

    updated add recipe functionality

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,26 @@
 $(document).ready(function(){
-    // materilize-css elements
+    // Materialize-css elements initialization
     //$('select').formSelect();
+    $('.tooltipped').tooltip();
 
+    // Adds new line for Preparation steps
+    let step = 1;
+    $('#add_prep_step').click(function(){
+        //console.log($('#recipe-form div.row').last().children().last().text())
+        $('#recipe-form div.row').eq(2).children().last().append(`
+            <a class="delete_step btn-floating btn-small waves-effect red tooltipped" data-position="left" data-tooltip="Delete"><i class="material-icons">delete_forever</i></a>
+            <textarea id="prep${step +=1}" name="prep${step}" class="materialize-textarea" required></textarea>
+        `);
 
+       // removes new line added when trash icon is clicked
+        $('.delete_step').click(function(){
+            $(this).next().remove();
+            $(this).remove();
+        });
+    });
+
+    
+
+    //$('#add_prep_step'). $("p").after()("Some appended text.");
 
 });
